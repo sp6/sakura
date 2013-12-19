430c720fefed1835213b6de61ae61d22631aad19
sp6 <dsspookies@gmail.com>
2013-12-19 12:59:03 +0900

    create: Kifu class
    Signed-off-by: sp6 <dsspookies@gmail.com>

--- lib/db/kifu.rb
+++ lib/db/kifu.rb
@@ -16,6 +16,83 @@ class Kifu
   
   def initialize
     @cur_idx = 0
+    @sashite = Array.new
+    @kyokumen = Kyokumen.new
+  end
+
+  def read(file_path)
+    File.foreach(file_path) do |line|
+      case line
+      when /^'.*/ # コメント
+        ;
+      when /^N\+(.*)/ # 先手
+        @sente = $1
+      when /^N\-(.*)/ # 後手
+        @gote = $1
+      when /\$EVENT:(.*)/ # 棋戦名
+        @event = $1
+      when /\$SITE:(.*)/ # 対局場所
+        @site = $1
+      when /\$START_TIME:(.*)/ # 対局開始日時
+        @start_time = $1
+      when /\$END_TIME:(.*)/ # 対局終了日時
+        @end_time = $1
+      when /\$TIME_LIMIT:(.*)/ # 持ち時間
+        @time_limit = $1
+      when /\$OPENING:(.*)/ # 戦型
+        @opening = $1
+      when /\$(.*):(.*)/ # 補足
+        @others ||= Hash.new
+        @others[$1] = $2
+      when /^\+(\d{2})(\d{2})(\w{2})/
+        from = $1
+        to = $2
+        koma = create($3)
+        @sashite << Te.new(:sente, Pos.new(from[0].to_i, from[1].to_i),
+                           Pos.new(to[0].to_i, to[1].to_i), koma)
+      when /^\-(\d{2})(\d{2})(\w{2})/
+        from = $1
+        to = $2
+        koma = create($3)
+        @sashite << Te.new(:gote, Pos.new(from[0].to_i, from[1].to_i),
+                           Pos.new(to[0].to_i, to[1].to_i), koma)
+      # 平手初期配置と駒落ち
+      end
+    end
+  end
+  
+  def create(csa_name)
+    case csa_name
+    when "FU"
+      Fu.new
+    when "KY"
+      Kyosha.new
+    when "KE"
+      Keima.new
+    when "GI"
+      Gin.new
+    when "KI"
+      Kin.new
+    when "KA"
+      Kaku.new
+    when "HI"
+      Hisha.new
+    when "OU"
+      Ou.new
+    when "TO"
+      To.new
+    when "NY"
+      Narikyo.new
+    when "NK"
+      Narikei.new
+    when "NG"
+      Narigin.new
+    when "UM"
+      Uma.new
+    when "RY"
+      Ryu.new
+    else
+    end
   end
   
   def forward
@@ -79,87 +156,12 @@ class Kifu
   end
 end
 
-def create(csa_name)
-  case csa_name
-  when "FU"
-    Fu.new
-  when "KY"
-    Kyosha.new
-  when "KE"
-    Keima.new
-  when "GI"
-    Gin.new
-  when "KI"
-    Kin.new
-  when "KA"
-    Kaku.new
-  when "HI"
-    Hisha.new
-  when "OU"
-    Ou.new
-  when "TO"
-    To.new
-  when "NY"
-    Narikyo.new
-  when "NK"
-    Narikei.new
-  when "NG"
-    Narigin.new
-  when "UM"
-    Uma.new
-  when "RY"
-    Ryu.new
-  else
-  end
-end
-
 dir_name = "../../data/"
 file_name = "example.csa"
 file_name = "Semifinal_tsutsukana+yss.csa"
 
 kifu = Kifu.new
-sashite = Array.new
-File.foreach(dir_name + file_name) do |line|
-  case line
-  when /^'.*/ # コメント
-    ;
-  when /^N\+(.*)/ # 先手
-    kifu.sente = $1
-  when /^N\-(.*)/ # 後手
-    kifu.gote = $1
-  when /\$EVENT:(.*)/ # 棋戦名
-    kifu.event = $1
-  when /\$SITE:(.*)/ # 対局場所
-    kifu.site = $1
-  when /\$START_TIME:(.*)/ # 対局開始日時
-    kifu.start_time = $1
-  when /\$END_TIME:(.*)/ # 対局終了日時
-    kifu.end_time = $1
-  when /\$TIME_LIMIT:(.*)/ # 持ち時間
-    kifu.time_limit = $1
-  when /\$OPENING:(.*)/ # 戦型
-    kifu.opening = $1
-  when /\$(.*):(.*)/ # 補足
-    kifu.others ||= Hash.new
-    kifu.others[$1] = $2
-  when /^\+(\d{2})(\d{2})(\w{2})/
-    from = $1
-    to = $2
-    koma = create($3)
-    sashite << Te.new(:sente, Pos.new(from[0].to_i, from[1].to_i),
-                       Pos.new(to[0].to_i, to[1].to_i), koma)
-  when /^\-(\d{2})(\d{2})(\w{2})/
-    from = $1
-    to = $2
-    koma = create($3)
-    sashite << Te.new(:gote, Pos.new(from[0].to_i, from[1].to_i),
-                       Pos.new(to[0].to_i, to[1].to_i), koma)
-  # 平手初期配置と駒落ち
-  end
-end
-
-kifu.sashite = sashite
-kifu.kyokumen = Kyokumen.new
+kifu.read(dir_name + file_name)
 kifu.console_view
 =begin
 kifu.sashite_each do |kyokumen, te, tesuu|
